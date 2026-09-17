8031ea046fa62caa2525323297c5a2c06f8e00fa
mbellos <mbellos2003@gmail.com>
2026-09-17 20:19:21 +0200

    spectral integration

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -4,9 +4,9 @@
 from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\EODP\eodp_students\auxiliary'
-indir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-ISM\input\gradient_alt100_act150" # small scene
-outdir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-ISM\myoutput"
+auxdir = r'C:\\Users\\mbell\\OneDrive\\Escritorio\\EODT\\EODT_Mario\\auxiliary'
+indir = r"C:\\Users\\mbell\\OneDrive\\Escritorio\\EODT\\SHARED\\EODP_TER_2021\\EODP-TS-ISM\\input\\gradient_alt100_act150" # small scene
+outdir = r"C:\\Users\\mbell\\OneDrive\\Escritorio\\EODT\\SHARED\\EODP_TER_2021\\EODP-TS-ISM\\output_Mario"
 
 # Initialise the ISM
 myIsm = ism(auxdir, indir, outdir)
--- ism/src/opticalPhase.py
+++ ism/src/opticalPhase.py
@@ -93,6 +93,8 @@ class opticalPhase(initIsm):
         :return: TOA image in irradiances [mW/m2]
         """
         # TODO
+
+
         return toa
 
 
@@ -115,6 +117,27 @@ class opticalPhase(initIsm):
         :return: TOA image 2D in radiances [mW/m2]
         """
         # TODO
+        isrf, wv_isrf = readIsrf(self.auxdir + '/' + self.ismConfig.isrffile, band)
+        toa=np.zeros((sgm_toa.shape[0], sgm_toa.shape[1])) # Integramos en la dimensión espectral
+
+        # El área del isrf normalizado debe ser 1
+        # Normalización
+        isrf=isrf/np.sum(isrf)
+        # Pasar a nm
+        wv_isrf_nm=wv_isrf*1000
+
+        # Interpolación del ISRF a las lamda de SGM
+        # LAS DOS SIGUIENTES LINEAS SON UNA ALTERNATIVA
+        #cs = interp1d(wv_isrf_nm, isrf, fill_value=(0, 0), bounds_error=False)
+        #interp_isrf = cs(sgm_wv)  # 1D vector
+
+        for ialt in range (sgm_toa.shape[0]): #ialt=i along track
+            for iact in range (sgm_toa.shape[1]): #iact=i across track
+                #toa[ialt,iact]=np.sum(sgm_toa[ialt,iact,:] * interp_isrf) # ALTERNATIVA
+                cs = interp1d(sgm_wv, sgm_toa[ialt, iact, :], fill_value=(0, 0), bounds_error=False)
+                sgm_inter=cs(wv_isrf_nm)
+                toa[ialt,iact]=np.sum(sgm_inter*isrf)
+
         return toa
 
 
